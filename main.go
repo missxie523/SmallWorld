7ca8d9419ffa94e142690ae4b05e15f2188ca956
missxie <missxie523@gmail.com>
2018-06-16 23:13:18 -0400

    update

--- main.go
+++ main.go
@@ -129,8 +129,7 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
 		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		ps = append(ps, p)
-
+		//ps = append(ps, p)
 	}
 	js, err := json.Marshal(ps)
 	if err != nil {
