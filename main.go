9e950b800f01a67e173af3e3b0f4ff701c2516f8
missxie <missxie523@gmail.com>
2018-07-03 00:24:08 -0400

    update 0703

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ const (
 	BT_INSTANCE = "smallworld-post-2-0617"
 	// Needs to update this URL if you deploy it to cloud.
 	//ES_URL = "http://35.224.38.226:9200"
-	ES_URL = "http://35.227.50.126:9200"
+	ES_URL = "http://35.227.44.3:9200"
 
 	// Needs to update this bucket based on your gcs bucket name.
 	BUCKET_NAME = "post-image-0616"
