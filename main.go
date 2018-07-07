d4bb1bd4f12b01477d7e0c2db963e8cc169d6e16
missxie <missxie523@gmail.com>
2018-07-07 02:53:15 -0400

    update cors

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 
 	//"golang.org/x/oauth2/jwt"
 	"cloud.google.com/go/bigtable"
+	"github.com/go-chi/cors"
 )
 
 type Location struct {
@@ -99,6 +100,18 @@ func main() {
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+	})
+	handler := c.Handler(r)
+
+	log.Println("Listening on port 8080")
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatal(err)
+	}
 
 }
 
